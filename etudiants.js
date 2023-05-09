7b5d904cc8d4ce764540574394b02ce41e71fcd5
Pedro Gonçalves <pedro.filipe.g@gmail.com>
2023-05-09 13:55:23 +0200

    first commit

---
+++ etudiants.js
@@ -0,0 +1,35 @@
+// Table des étudiants
+// *******************
+const students = [
+    [54, 'Durand'],
+    [55, 'Dupond'],
+    [3, 'Smith'],
+    [28, 'Toto']
+];
+
+// Table des notes par etudiant
+// ****************************
+const ranges = [
+    [54,[12, 14, 17]],
+    [55, [13, 4, 19]],
+    [3, [10, 10, 10]]
+];
+
+// Table des cours
+// ***************
+const courses = [
+    [1, "algo"], 
+    [2, "math"], 
+    [3, "anglais"], 
+    [12, "web"],
+    [25, "macramé"]
+];
+
+// Table des cours par étudiant
+// ****************************
+const coursesByStudent = [
+    [54, [1, 2, 3]], 
+    [55, [1, 3]], 
+    [3, [2, 3, 12]]
+];
+
